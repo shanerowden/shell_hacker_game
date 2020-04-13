a2da2a23933f7b536a8624fc98451e4c7238a369
Just Another One <mockidlat@protonmail.com>
2020-04-12 23:44:15 -0500

    unfucked database

--- migrations/env.py
+++ migrations/env.py
@@ -8,6 +8,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from shellmancer.models import *
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
